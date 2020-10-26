d1ec860d5fc458b434ce112e07ee21bb2feedd04
llllhy <963785820@qq.com>
2020-10-26 10:04:37 +0800

    modify RGB and remind staring way

--- plugin/awaken_project_plugin.py
+++ plugin/awaken_project_plugin.py
@@ -5,7 +5,8 @@ class Plugin(AbstractPlugin):
 
     def handle(self, text, parsed):
         self.say('已经在启动啦!', cache=True)
-        os.system('sudo python3 ~/Travel-1/start_project.py')
+        os.system('python3 ~/Travel-1/start_project.py')
+        os.system('sudo python3 ~/Travel-1/RGB.py')
         self.say('启动完成，放心地出门吧!', cache=True)
 
     def isValid(self, text, parsed):
